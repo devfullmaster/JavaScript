dc73b9706f516cc1f65c589c5267a0be89553b24
Rudy Almeida Silva <devfullmaster@gmail.com>
2022-10-17 22:10:42 -0300

    JavaScrip Exercicios finalizado

--- 03 - lista de exercios/desafio.js
+++ 03 - lista de exercios/desafio.js
@@ -70,3 +70,18 @@ console.log(imc);
     - Em duas vezes, preço normal de etiqueta sem juros;
     - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
 */
+
+const precoEtiqueta = 100;
+const formaDePagamento = 2;
+
+if (formaDePagamento === 1) {
+    console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+} else if (formaDePagamento === 2) {
+    console.log(precoEtiqueta - (precoEtiqueta * 0.15));
+} else if (formaDePagamento === 3) {
+    console.log(precoEtiqueta);
+} else {
+    console.log(precoEtiqueta + (precoEtiqueta * 0.1));
+}
+
+
